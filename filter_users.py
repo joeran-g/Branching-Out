3f0c9cf005d1f1bfb6b151864fa25062c78c91de
Jöran Gläser <Joeran.glaeser@gmail.com>
2025-11-18 15:48:06 +0100

    implemented PEP 8 formatting and comments

--- filter_users.py
+++ filter_users.py
@@ -2,6 +2,7 @@ import json
 
 
 def filter_users_by_name(name):
+    """ Take a <name> as str and return user-data(dict) with the user-name -> <name> """
     with open("users.json", "r") as file:
         users = json.load(file)
 
@@ -12,6 +13,7 @@ def filter_users_by_name(name):
 
 
 def filter_users_by_age(age):
+    """ Take a <age> as int and return user-data(dict) with the user-age -> <age> """
     with open("users.json", "r") as file:
         users = json.load(file)
     filtered_by_age = [user for user in users if user['age'] == age]
@@ -21,6 +23,10 @@ def filter_users_by_age(age):
 
 
 def filter_users_by_email(email):
+    """
+    Take a <email> as str and return user-data(dict) with the user-mail -> <email>
+    filters via ending of the addresses: '.net', '.com', ...
+    """
     with open("users.json", "r") as file:
         users = json.load(file)
     filtered_by_email = [user for user in users if user['email'].strip().endswith(email)]
@@ -30,6 +36,12 @@ def filter_users_by_email(email):
 
 
 if __name__ == "__main__":
+    """
+    Ask the user what he wants to filter by and return user-data(dict) that the user wants to see.
+    name -> string (case-insensitive, exact name)
+    age -> int (a positive number)
+    email -> string (.com, .net, ...)
+    """
     filter_option = input("What would you like to filter by? (name/age/email): ").strip().lower()
 
     if filter_option == "name":
@@ -38,9 +50,11 @@ if __name__ == "__main__":
     elif filter_option == "age":
         try:
             age_to_search = int(input("Enter an age to filter by: ").strip())
+            if age_to_search < 1:
+                raise ValueError
             filter_users_by_age(age_to_search)
         except ValueError:
-            print("\nno valid age to searh for\n")
+            print("\nno valid age to search for\n")
     elif filter_option == "email":
         email_to_search = input("Enter a mail to filter by (.com/.net/...): ")
         filter_users_by_email(email_to_search)
